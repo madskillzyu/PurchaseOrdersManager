3dfc33c582c2df223f77ec9c02bc123802560418
Марко Лазић <madskillz.yu@hotmail.com>
2020-02-05 11:37:51 +0100

    #dizajn Dodat menu strip sa opcijama.

--- PurchaseOrdersManager/DashBoard.Designer.cs
+++ PurchaseOrdersManager/DashBoard.Designer.cs
@@ -28,7 +28,281 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.podešavanjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filteriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kontrolaNalogaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bazaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.izmeniPutanjuDoBazeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.izmeniPutanjuDoFajlovaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.istorijaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.neStarijeOd45DanaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.neStarijeOd60DanaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mesecToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.januarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.februarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.martToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aprilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.majToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.junToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.julToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.avgustToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.septembarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oktobarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.novembarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.decembarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.izvestajPoslatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jesteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nijeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sveZaTekućuGodinuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sveOdPrvogUnosaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.prikažiGrupeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bugReportFeatureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.podešavanjaToolStripMenuItem,
+            this.filteriToolStripMenuItem,
+            this.infoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1593, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // podešavanjaToolStripMenuItem
+            // 
+            this.podešavanjaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kontrolaNalogaToolStripMenuItem,
+            this.bazaToolStripMenuItem,
+            this.istorijaToolStripMenuItem});
+            this.podešavanjaToolStripMenuItem.Name = "podešavanjaToolStripMenuItem";
+            this.podešavanjaToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.podešavanjaToolStripMenuItem.Text = "Podešavanja";
+            // 
+            // filteriToolStripMenuItem
+            // 
+            this.filteriToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.neStarijeOd45DanaToolStripMenuItem,
+            this.neStarijeOd60DanaToolStripMenuItem,
+            this.mesecToolStripMenuItem,
+            this.izvestajPoslatToolStripMenuItem,
+            this.sveToolStripMenuItem,
+            this.prikažiGrupeToolStripMenuItem});
+            this.filteriToolStripMenuItem.Name = "filteriToolStripMenuItem";
+            this.filteriToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.filteriToolStripMenuItem.Text = "Filteri";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.bugReportFeatureToolStripMenuItem});
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.infoToolStripMenuItem.Text = "Info";
+            // 
+            // kontrolaNalogaToolStripMenuItem
+            // 
+            this.kontrolaNalogaToolStripMenuItem.Name = "kontrolaNalogaToolStripMenuItem";
+            this.kontrolaNalogaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kontrolaNalogaToolStripMenuItem.Text = "Kontrola Naloga";
+            // 
+            // bazaToolStripMenuItem
+            // 
+            this.bazaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.izmeniPutanjuDoBazeToolStripMenuItem,
+            this.izmeniPutanjuDoFajlovaToolStripMenuItem});
+            this.bazaToolStripMenuItem.Name = "bazaToolStripMenuItem";
+            this.bazaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.bazaToolStripMenuItem.Text = "Baza";
+            // 
+            // izmeniPutanjuDoBazeToolStripMenuItem
+            // 
+            this.izmeniPutanjuDoBazeToolStripMenuItem.Name = "izmeniPutanjuDoBazeToolStripMenuItem";
+            this.izmeniPutanjuDoBazeToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.izmeniPutanjuDoBazeToolStripMenuItem.Text = "Izmeni putanju do baze";
+            // 
+            // izmeniPutanjuDoFajlovaToolStripMenuItem
+            // 
+            this.izmeniPutanjuDoFajlovaToolStripMenuItem.Name = "izmeniPutanjuDoFajlovaToolStripMenuItem";
+            this.izmeniPutanjuDoFajlovaToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.izmeniPutanjuDoFajlovaToolStripMenuItem.Text = "Izmeni putanju do fajlova";
+            // 
+            // istorijaToolStripMenuItem
+            // 
+            this.istorijaToolStripMenuItem.Name = "istorijaToolStripMenuItem";
+            this.istorijaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.istorijaToolStripMenuItem.Text = "Istorija";
+            // 
+            // neStarijeOd45DanaToolStripMenuItem
+            // 
+            this.neStarijeOd45DanaToolStripMenuItem.Name = "neStarijeOd45DanaToolStripMenuItem";
+            this.neStarijeOd45DanaToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.neStarijeOd45DanaToolStripMenuItem.Text = "Ne starije od 45 dana";
+            // 
+            // neStarijeOd60DanaToolStripMenuItem
+            // 
+            this.neStarijeOd60DanaToolStripMenuItem.Name = "neStarijeOd60DanaToolStripMenuItem";
+            this.neStarijeOd60DanaToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.neStarijeOd60DanaToolStripMenuItem.Text = "Ne starije od 60 dana";
+            // 
+            // mesecToolStripMenuItem
+            // 
+            this.mesecToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.januarToolStripMenuItem,
+            this.februarToolStripMenuItem,
+            this.martToolStripMenuItem,
+            this.aprilToolStripMenuItem,
+            this.majToolStripMenuItem,
+            this.junToolStripMenuItem,
+            this.julToolStripMenuItem,
+            this.avgustToolStripMenuItem,
+            this.septembarToolStripMenuItem,
+            this.oktobarToolStripMenuItem,
+            this.novembarToolStripMenuItem,
+            this.decembarToolStripMenuItem});
+            this.mesecToolStripMenuItem.Name = "mesecToolStripMenuItem";
+            this.mesecToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.mesecToolStripMenuItem.Text = "Mesec";
+            // 
+            // januarToolStripMenuItem
+            // 
+            this.januarToolStripMenuItem.Name = "januarToolStripMenuItem";
+            this.januarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.januarToolStripMenuItem.Text = "Januar";
+            // 
+            // februarToolStripMenuItem
+            // 
+            this.februarToolStripMenuItem.Name = "februarToolStripMenuItem";
+            this.februarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.februarToolStripMenuItem.Text = "Februar";
+            // 
+            // martToolStripMenuItem
+            // 
+            this.martToolStripMenuItem.Name = "martToolStripMenuItem";
+            this.martToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.martToolStripMenuItem.Text = "Mart";
+            // 
+            // aprilToolStripMenuItem
+            // 
+            this.aprilToolStripMenuItem.Name = "aprilToolStripMenuItem";
+            this.aprilToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aprilToolStripMenuItem.Text = "April";
+            // 
+            // majToolStripMenuItem
+            // 
+            this.majToolStripMenuItem.Name = "majToolStripMenuItem";
+            this.majToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.majToolStripMenuItem.Text = "Maj";
+            // 
+            // junToolStripMenuItem
+            // 
+            this.junToolStripMenuItem.Name = "junToolStripMenuItem";
+            this.junToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.junToolStripMenuItem.Text = "Jun";
+            // 
+            // julToolStripMenuItem
+            // 
+            this.julToolStripMenuItem.Name = "julToolStripMenuItem";
+            this.julToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.julToolStripMenuItem.Text = "Jul";
+            // 
+            // avgustToolStripMenuItem
+            // 
+            this.avgustToolStripMenuItem.Name = "avgustToolStripMenuItem";
+            this.avgustToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.avgustToolStripMenuItem.Text = "Avgust";
+            // 
+            // septembarToolStripMenuItem
+            // 
+            this.septembarToolStripMenuItem.Name = "septembarToolStripMenuItem";
+            this.septembarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.septembarToolStripMenuItem.Text = "Septembar";
+            // 
+            // oktobarToolStripMenuItem
+            // 
+            this.oktobarToolStripMenuItem.Name = "oktobarToolStripMenuItem";
+            this.oktobarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.oktobarToolStripMenuItem.Text = "Oktobar";
+            // 
+            // novembarToolStripMenuItem
+            // 
+            this.novembarToolStripMenuItem.Name = "novembarToolStripMenuItem";
+            this.novembarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.novembarToolStripMenuItem.Text = "Novembar";
+            // 
+            // decembarToolStripMenuItem
+            // 
+            this.decembarToolStripMenuItem.Name = "decembarToolStripMenuItem";
+            this.decembarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.decembarToolStripMenuItem.Text = "Decembar";
+            // 
+            // izvestajPoslatToolStripMenuItem
+            // 
+            this.izvestajPoslatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jesteToolStripMenuItem,
+            this.nijeToolStripMenuItem});
+            this.izvestajPoslatToolStripMenuItem.Name = "izvestajPoslatToolStripMenuItem";
+            this.izvestajPoslatToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.izvestajPoslatToolStripMenuItem.Text = "Izvestaj Poslat";
+            // 
+            // jesteToolStripMenuItem
+            // 
+            this.jesteToolStripMenuItem.Name = "jesteToolStripMenuItem";
+            this.jesteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.jesteToolStripMenuItem.Text = "Jeste";
+            // 
+            // nijeToolStripMenuItem
+            // 
+            this.nijeToolStripMenuItem.Name = "nijeToolStripMenuItem";
+            this.nijeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nijeToolStripMenuItem.Text = "Nije";
+            // 
+            // sveToolStripMenuItem
+            // 
+            this.sveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sveZaTekućuGodinuToolStripMenuItem,
+            this.sveOdPrvogUnosaToolStripMenuItem});
+            this.sveToolStripMenuItem.Name = "sveToolStripMenuItem";
+            this.sveToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.sveToolStripMenuItem.Text = "Sve";
+            // 
+            // sveZaTekućuGodinuToolStripMenuItem
+            // 
+            this.sveZaTekućuGodinuToolStripMenuItem.Name = "sveZaTekućuGodinuToolStripMenuItem";
+            this.sveZaTekućuGodinuToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.sveZaTekućuGodinuToolStripMenuItem.Text = "Sve za tekuću godinu";
+            // 
+            // sveOdPrvogUnosaToolStripMenuItem
+            // 
+            this.sveOdPrvogUnosaToolStripMenuItem.Name = "sveOdPrvogUnosaToolStripMenuItem";
+            this.sveOdPrvogUnosaToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.sveOdPrvogUnosaToolStripMenuItem.Text = "Sve od prvog unosa";
+            // 
+            // prikažiGrupeToolStripMenuItem
+            // 
+            this.prikažiGrupeToolStripMenuItem.Name = "prikažiGrupeToolStripMenuItem";
+            this.prikažiGrupeToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.prikažiGrupeToolStripMenuItem.Text = "Prikaži grupe";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // bugReportFeatureToolStripMenuItem
+            // 
+            this.bugReportFeatureToolStripMenuItem.Name = "bugReportFeatureToolStripMenuItem";
+            this.bugReportFeatureToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.bugReportFeatureToolStripMenuItem.Text = "Bug Report/Feature";
             // 
             // Dashboard
             // 
@@ -36,15 +310,54 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1593, 809);
+            this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Dashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DashBoard";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem podešavanjaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kontrolaNalogaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bazaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem izmeniPutanjuDoBazeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem izmeniPutanjuDoFajlovaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem istorijaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filteriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem neStarijeOd45DanaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem neStarijeOd60DanaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mesecToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem januarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem februarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem martToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aprilToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem majToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem junToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem julToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem avgustToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem septembarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oktobarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem novembarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem decembarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem izvestajPoslatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jesteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nijeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sveZaTekućuGodinuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sveOdPrvogUnosaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem prikažiGrupeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bugReportFeatureToolStripMenuItem;
     }
 }
 
