654a4e41c6bc906912f34489e89e80a99d397525
Марко Лазић <madskillz.yu@hotmail.com>
2020-02-06 20:22:35 +0100

    #dizajn Dodat about box, i dodati podaci o aplikaciji u Assembly Information( desni klik na project pa onda properties)

--- PurchaseOrdersManager/Properties/AssemblyInfo.cs
+++ PurchaseOrdersManager/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PurchaseOrdersManager")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This is mangment app for monitoring and manipulation orders.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MadskillzSolutions")]
 [assembly: AssemblyProduct("PurchaseOrdersManager")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2020 Mарко Лазић All Rights Reserved")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
