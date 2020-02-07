9b4b31de5ffc54af20201ff6d9926e4c01825b45
Марко Лазић <madskillz.yu@hotmail.com>
2020-02-07 12:21:03 +0100

    #dizajn Dodata forma History i komande na njoj

---
+++ PurchaseOrdersManager/History.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace PurchaseOrdersManager
+{
+    partial class History
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(History));
+            this.objectListView1 = new BrightIdeasSoftware.ObjectListView();
+            this.ID = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn1 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn2 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn3 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn4 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn5 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn6 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn7 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn8 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn9 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn10 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn11 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn12 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn13 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn14 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn15 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn16 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn17 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn18 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            ((System.ComponentModel.ISupportInitialize)(this.objectListView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // objectListView1
+            // 
+            this.objectListView1.AllColumns.Add(this.ID);
+            this.objectListView1.AllColumns.Add(this.olvColumn1);
+            this.objectListView1.AllColumns.Add(this.olvColumn2);
+            this.objectListView1.AllColumns.Add(this.olvColumn3);
+            this.objectListView1.AllColumns.Add(this.olvColumn4);
+            this.objectListView1.AllColumns.Add(this.olvColumn5);
+            this.objectListView1.AllColumns.Add(this.olvColumn6);
+            this.objectListView1.AllColumns.Add(this.olvColumn7);
+            this.objectListView1.AllColumns.Add(this.olvColumn8);
+            this.objectListView1.AllColumns.Add(this.olvColumn9);
+            this.objectListView1.AllColumns.Add(this.olvColumn10);
+            this.objectListView1.AllColumns.Add(this.olvColumn11);
+            this.objectListView1.AllColumns.Add(this.olvColumn12);
+            this.objectListView1.AllColumns.Add(this.olvColumn13);
+            this.objectListView1.AllColumns.Add(this.olvColumn14);
+            this.objectListView1.AllColumns.Add(this.olvColumn15);
+            this.objectListView1.AllColumns.Add(this.olvColumn16);
+            this.objectListView1.AllColumns.Add(this.olvColumn17);
+            this.objectListView1.AllColumns.Add(this.olvColumn18);
+            this.objectListView1.CellEditUseWholeCell = false;
+            this.objectListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.olvColumn1,
+            this.olvColumn2,
+            this.olvColumn3,
+            this.olvColumn4,
+            this.olvColumn5,
+            this.olvColumn6,
+            this.olvColumn7,
+            this.olvColumn8,
+            this.olvColumn9,
+            this.olvColumn10,
+            this.olvColumn11,
+            this.olvColumn12,
+            this.olvColumn13,
+            this.olvColumn14,
+            this.olvColumn15,
+            this.olvColumn16,
+            this.olvColumn17,
+            this.olvColumn18});
+            this.objectListView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.objectListView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.objectListView1.FullRowSelect = true;
+            this.objectListView1.GridLines = true;
+            this.objectListView1.HideSelection = false;
+            this.objectListView1.Location = new System.Drawing.Point(0, 0);
+            this.objectListView1.MultiSelect = false;
+            this.objectListView1.Name = "objectListView1";
+            this.objectListView1.Size = new System.Drawing.Size(800, 450);
+            this.objectListView1.TabIndex = 1;
+            this.objectListView1.UseCompatibleStateImageBehavior = false;
+            this.objectListView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ID
+            // 
+            this.ID.AspectName = "ID";
+            this.ID.Text = "ID";
+            // 
+            // olvColumn1
+            // 
+            this.olvColumn1.AspectName = "DatumNarudzbenice";
+            this.olvColumn1.Text = "DatumNarudzbenice";
+            // 
+            // olvColumn2
+            // 
+            this.olvColumn2.AspectName = "DatumIstekaRokaNarudzbenice";
+            this.olvColumn2.Text = "DatumIstekaRokaNarudzbenice";
+            // 
+            // olvColumn3
+            // 
+            this.olvColumn3.AspectName = "OkvirniSporazum";
+            this.olvColumn3.Text = "OkvirniSporazum";
+            // 
+            // olvColumn4
+            // 
+            this.olvColumn4.AspectName = "Ogranak";
+            this.olvColumn4.Text = "Ogranak";
+            // 
+            // olvColumn5
+            // 
+            this.olvColumn5.AspectName = "NazivTrafostanice";
+            this.olvColumn5.Text = "NazivTrafostanice";
+            // 
+            // olvColumn6
+            // 
+            this.olvColumn6.AspectName = "DatumFakture";
+            this.olvColumn6.Text = "DatumFakture";
+            // 
+            // olvColumn7
+            // 
+            this.olvColumn7.AspectName = "BrojFakture";
+            this.olvColumn7.Text = "BrojFakture";
+            // 
+            // olvColumn8
+            // 
+            this.olvColumn8.AspectName = "IznosBezPDV";
+            this.olvColumn8.Text = "IznosBezPDV";
+            // 
+            // olvColumn9
+            // 
+            this.olvColumn9.AspectName = "DatumPrijemEPS";
+            this.olvColumn9.Text = "DatumPrijemEPS";
+            // 
+            // olvColumn10
+            // 
+            this.olvColumn10.AspectName = "BrojIzvestaja";
+            this.olvColumn10.Text = "BrojIzvestaja";
+            // 
+            // olvColumn11
+            // 
+            this.olvColumn11.AspectName = "Napomena";
+            this.olvColumn11.FillsFreeSpace = true;
+            this.olvColumn11.Text = "Napomena";
+            // 
+            // olvColumn12
+            // 
+            this.olvColumn12.AspectName = "IzvestajPoslat";
+            this.olvColumn12.Text = "IzvestajPoslat";
+            // 
+            // olvColumn13
+            // 
+            this.olvColumn13.AspectName = "NarudzbenicaPDF";
+            this.olvColumn13.FillsFreeSpace = true;
+            this.olvColumn13.Text = "NarudzbenicaPDF";
+            // 
+            // olvColumn14
+            // 
+            this.olvColumn14.AspectName = "ZapsinikOIzvrsenjuUslugaPDF";
+            this.olvColumn14.FillsFreeSpace = true;
+            this.olvColumn14.Text = "ZapsinikOIzvrsenjuUslugaPDF";
+            // 
+            // olvColumn15
+            // 
+            this.olvColumn15.AspectName = "FakturaPDF";
+            this.olvColumn15.FillsFreeSpace = true;
+            this.olvColumn15.Text = "FakturaPDF";
+            // 
+            // olvColumn16
+            // 
+            this.olvColumn16.AspectName = "PrimopredajniObrazacPDF";
+            this.olvColumn16.FillsFreeSpace = true;
+            this.olvColumn16.Text = "PrimopredajniObrazacPDF";
+            // 
+            // olvColumn17
+            // 
+            this.olvColumn17.AspectName = "Korisnik";
+            this.olvColumn17.Text = "Korisnik";
+            // 
+            // olvColumn18
+            // 
+            this.olvColumn18.AspectName = "Datum";
+            this.olvColumn18.Text = "Datum";
+            // 
+            // History
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.objectListView1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "History";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "History";
+            ((System.ComponentModel.ISupportInitialize)(this.objectListView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private BrightIdeasSoftware.ObjectListView objectListView1;
+        private BrightIdeasSoftware.OLVColumn ID;
+        private BrightIdeasSoftware.OLVColumn olvColumn1;
+        private BrightIdeasSoftware.OLVColumn olvColumn2;
+        private BrightIdeasSoftware.OLVColumn olvColumn3;
+        private BrightIdeasSoftware.OLVColumn olvColumn4;
+        private BrightIdeasSoftware.OLVColumn olvColumn5;
+        private BrightIdeasSoftware.OLVColumn olvColumn6;
+        private BrightIdeasSoftware.OLVColumn olvColumn7;
+        private BrightIdeasSoftware.OLVColumn olvColumn8;
+        private BrightIdeasSoftware.OLVColumn olvColumn9;
+        private BrightIdeasSoftware.OLVColumn olvColumn10;
+        private BrightIdeasSoftware.OLVColumn olvColumn11;
+        private BrightIdeasSoftware.OLVColumn olvColumn12;
+        private BrightIdeasSoftware.OLVColumn olvColumn13;
+        private BrightIdeasSoftware.OLVColumn olvColumn14;
+        private BrightIdeasSoftware.OLVColumn olvColumn15;
+        private BrightIdeasSoftware.OLVColumn olvColumn16;
+        private BrightIdeasSoftware.OLVColumn olvColumn17;
+        private BrightIdeasSoftware.OLVColumn olvColumn18;
+    }
+}
