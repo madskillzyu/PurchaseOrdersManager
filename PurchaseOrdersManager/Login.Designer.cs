960ce1a647716b68181e8db3d75226ac7e2bc233
Марко Лазић <madskillz.yu@hotmail.com>
2020-02-08 10:54:17 +0100

    #dizajn dodat logo na login formi i u resources folderu

--- PurchaseOrdersManager/Login.Designer.cs
+++ PurchaseOrdersManager/Login.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.labelLozinka = new System.Windows.Forms.Label();
             this.labelKorisnickoIme = new System.Windows.Forms.Label();
@@ -39,9 +40,11 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(95, 27);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(419, 263);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 17;
             this.pictureBox1.TabStop = false;
             // 
