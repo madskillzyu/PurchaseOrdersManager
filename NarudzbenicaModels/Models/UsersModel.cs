fa4c9b88fe57611889369b73c1395735d1810b16
Марко Лазић <madskillz.yu@hotmail.com>
2020-02-08 11:21:10 +0100

    #classlibrary Aded UsersModel

---
+++ NarudzbenicaModels/Models/UsersModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NarudzbenicaModels.Models
+{
+    class UsersModel
+    {
+        public  int ID { get; set; }
+        public String UserName { get; set; }
+        public String UserPassword { get; set; }
+        public String Permission { get; set; } // It should be user level acces admin or user
+        public DateTime Date { get; set; }
+    }
+}
