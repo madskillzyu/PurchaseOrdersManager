7dedf30ac4c496854b50be31c5b4bb0358e82f36
Марко Лазић <madskillz.yu@hotmail.com>
2020-02-11 14:51:12 +0100

    #classlibrary added constructor to PurchaseOrderModel

--- NarudzbenicaModels/Models/PurchaseOrderModel.cs
+++ NarudzbenicaModels/Models/PurchaseOrderModel.cs
@@ -10,8 +10,8 @@ namespace NarudzbenicaModels.Models
     {
         public int ID { get; set; }
 
-        public String NumberOfPurchaseOrder { get; set; }
 
+        public String NumberOfPurchaseOrder { get; set; }
         public DateTime DateOfPurchaseOrder { get; set; }
         public DateTime ExpirationDateOfPurchaseOrder { get; set; }
         public String FrameworkAgreement { get; set; }
@@ -23,12 +23,41 @@ namespace NarudzbenicaModels.Models
         public DateTime DateOfAdmissionEPS { get; set; }
         public String ReportNumber { get; set; }
         public String Note { get; set; }
-        public int ReportIsSent { get; set; }
+        public int ReportIsSent { get; set; } // should be boolean
         public String PurchaseOrderPDF { get; set; }
         public String ServicePerformanceRecord { get; set; }
         public String InvoicePDF { get; set; }
         public String HandoverForm { get; set; }
         public String User { get; set; }
         public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public PurchaseOrderModel(String numberOfPurchaseOrder, DateTime dateOfPurchaseOrder, DateTime expirationDateOfPurchaseOrder, String frameworkAgreement,
+            String branch, String nameOfSubstation, DateTime invoiceDate, String invoiceNumber, String amountExcludingVAT, DateTime dateOfAdmissionEPS,
+            String reportNumber, String note, int reportIsSent, String purchaseOrderPdf, String servicePerformanceRecord, String invoicePDF, String handoverForm, String user, DateTime date )
+        {
+            this.NumberOfPurchaseOrder = numberOfPurchaseOrder;
+            this.DateOfPurchaseOrder = dateOfPurchaseOrder;
+            this.ExpirationDateOfPurchaseOrder = expirationDateOfPurchaseOrder;
+            this.FrameworkAgreement = frameworkAgreement;
+            this.Branch = branch;
+            this.NameOfSubstation = nameOfSubstation;
+            this.InvoiceDate = invoiceDate;
+            this.InvoiceNumber = invoiceNumber;
+            this.AmountExcludingVAT = amountExcludingVAT;
+            this.DateOfAdmissionEPS = dateOfAdmissionEPS;
+            this.ReportNumber = reportNumber;
+            this.Note = note;
+            this.ReportIsSent = reportIsSent;
+            this.PurchaseOrderPDF = purchaseOrderPdf;
+            this.ServicePerformanceRecord = servicePerformanceRecord;
+            this.InvoicePDF = invoicePDF;
+            this.HandoverForm = handoverForm;
+            this.User = user;
+            this.Date = date;
+
+        }
     }
 }
