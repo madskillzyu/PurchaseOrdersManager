7c4b001ef7a0e82ae364ec9591e83431446b3dba
Марко Лазић <madskillz.yu@hotmail.com>
2020-02-13 12:40:56 +0100

    #classlibrary Created PositionModel

---
+++ NarudzbenicaModels/Models/PositionModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NarudzbenicaModels.Models
+{
+    class PositionModel
+    {
+        public int PositionID { get; set; }
+        public int PurchaseOrder_ID { get; set; }
+        public String PositionName { get; set; }
+
+        public PositionModel()
+        {
+
+        }
+
+        public PositionModel(string positionName)
+        {
+            this.PositionName = positionName;
+        }
+    }
+}
