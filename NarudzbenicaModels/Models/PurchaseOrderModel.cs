0c618a6bb6553ef14cc0c990eb402e01c934b9df
Марко Лазић <madskillz.yu@hotmail.com>
2020-02-13 12:24:22 +0100

    #classlibrary Aded constructor to Purchase order model class

--- NarudzbenicaModels/Models/PurchaseOrderModel.cs
+++ NarudzbenicaModels/Models/PurchaseOrderModel.cs
@@ -32,12 +32,67 @@ namespace NarudzbenicaModels.Models
         public DateTime Date { get; set; }
 
         /// <summary>
-        /// Constructor
+        /// Constructor without id field
         /// </summary>
         public PurchaseOrderModel(String numberOfPurchaseOrder, DateTime dateOfPurchaseOrder, DateTime expirationDateOfPurchaseOrder, String frameworkAgreement,
             String branch, String nameOfSubstation, DateTime invoiceDate, String invoiceNumber, String amountExcludingVAT, DateTime dateOfAdmissionEPS,
             String reportNumber, String note, int reportIsSent, String purchaseOrderPdf, String servicePerformanceRecord, String invoicePDF, String handoverForm, String user, DateTime date )
         {
+            this.NumberOfPurchaseOrder = numberOfPurchaseOrder;
+            this.DateOfPurchaseOrder = dateOfPurchaseOrder;
+            this.ExpirationDateOfPurchaseOrder = expirationDateOfPurchaseOrder;
+            this.FrameworkAgreement = frameworkAgreement;
+            this.Branch = branch;
+            this.NameOfSubstation = nameOfSubstation;
+            this.InvoiceDate = invoiceDate;
+            this.InvoiceNumber = invoiceNumber;
+            this.AmountExcludingVAT = amountExcludingVAT;
+            this.DateOfAdmissionEPS = dateOfAdmissionEPS;
+            this.ReportNumber = reportNumber;
+            this.Note = note;
+            this.ReportIsSent = reportIsSent;
+            this.PurchaseOrderPDF = purchaseOrderPdf;
+            this.ServicePerformanceRecord = servicePerformanceRecord;
+            this.InvoicePDF = invoicePDF;
+            this.HandoverForm = handoverForm;
+            this.User = user;
+            this.Date = date;
+
+        }
+        /// <summary>
+        /// Construktor
+        /// </summary>
+        public PurchaseOrderModel()
+        {
+
+        }
+        /// <summary>
+        /// Construktor with ID field
+        /// </summary>
+        /// <param name="numberOfPurchaseOrder"></param>
+        /// <param name="dateOfPurchaseOrder"></param>
+        /// <param name="expirationDateOfPurchaseOrder"></param>
+        /// <param name="frameworkAgreement"></param>
+        /// <param name="branch"></param>
+        /// <param name="nameOfSubstation"></param>
+        /// <param name="invoiceDate"></param>
+        /// <param name="invoiceNumber"></param>
+        /// <param name="amountExcludingVAT"></param>
+        /// <param name="dateOfAdmissionEPS"></param>
+        /// <param name="reportNumber"></param>
+        /// <param name="note"></param>
+        /// <param name="reportIsSent"></param>
+        /// <param name="purchaseOrderPdf"></param>
+        /// <param name="servicePerformanceRecord"></param>
+        /// <param name="invoicePDF"></param>
+        /// <param name="handoverForm"></param>
+        /// <param name="user"></param>
+        /// <param name="date"></param>
+        public PurchaseOrderModel(int id, String numberOfPurchaseOrder, DateTime dateOfPurchaseOrder, DateTime expirationDateOfPurchaseOrder, String frameworkAgreement,
+            String branch, String nameOfSubstation, DateTime invoiceDate, String invoiceNumber, String amountExcludingVAT, DateTime dateOfAdmissionEPS,
+            String reportNumber, String note, int reportIsSent, String purchaseOrderPdf, String servicePerformanceRecord, String invoicePDF, String handoverForm, String user, DateTime date)
+        {
+            this.ID = id;
             this.NumberOfPurchaseOrder = numberOfPurchaseOrder;
             this.DateOfPurchaseOrder = dateOfPurchaseOrder;
             this.ExpirationDateOfPurchaseOrder = expirationDateOfPurchaseOrder;
