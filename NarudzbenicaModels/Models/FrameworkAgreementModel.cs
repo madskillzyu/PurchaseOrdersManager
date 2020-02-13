7b1a2fe294c533357e4413983f9022e7e0e4eaea
Марко Лазић <madskillz.yu@hotmail.com>
2020-02-13 12:36:03 +0100

    #classlibrary Crearted FrameworkAgreementModel mode, wirh constructors

---
+++ NarudzbenicaModels/Models/FrameworkAgreementModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NarudzbenicaModels.Models
+{
+    class FrameworkAgreementModel
+    {
+        public String City { get; set; }
+        public String NumberOfFrameworkAgreement { get; set; }
+
+        public FrameworkAgreementModel()
+        {
+
+        }
+
+        public FrameworkAgreementModel(String city, String numberOfFrameworkAgreement)
+        {
+            this.City = city;
+            this.NumberOfFrameworkAgreement = numberOfFrameworkAgreement;
+        }
+
+        /// <summary>
+        /// Property for returnig new string which consist og City name and the number of fremework agreement
+        /// </summary>
+        public String NAmeOfFrameworkAgreement
+        {
+            get
+            {
+                return $"{City} - {NumberOfFrameworkAgreement}";
+            }
+        }
+    }
+}
