e45aa8b28ae116b672d063189e797735777b3067
Марко Лазић <madskillz.yu@hotmail.com>
2020-02-29 11:49:10 +0100

    Added all functions from interface IDataConnection

--- NarudzbenicaModels/DataAccess/SQLiteConnector.cs
+++ NarudzbenicaModels/DataAccess/SQLiteConnector.cs
@@ -8,7 +8,116 @@ using NarudzbenicaModels.Models;
 
 namespace NarudzbenicaModels.DataAccess
 {
-    public class SQLiteConnector 
+    public class SQLiteConnector : IDataConnection
     {
+        public UsersModel CreateUser(UsersModel u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DatabaseEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime DateOfFirstInsertInDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PurchaseOrderModel DatOfLastInsert(string y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(PurchaseOrderModel narudzbenica)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PurchaseOrderModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PurchaseOrderModel> GetAllForThisYear(string y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PurchaseOrderModel> GetAllFromLastMonth(string m)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PurchaseOrderModel> GetAllOlderThen(int days)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<UsersModel> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PurchaseOrderModel> GetAllWhereIzvestajPoslat(int poslat, string y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PurchaseOrderModel GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PurchaseOrderModel NapraviNarudzbenicu(PurchaseOrderModel narudzbenica)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PositionModel NapraviPoziciju(PositionModel pozicija)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PurchaseOrederPositionModel> NarudbeniceSaPozicijam()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SumaIzvestajPosla(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SumaKompletiranih()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UkupanBorjUNosaOvogMeseca(string mesec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UkupanBrojUnosa()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UkupanBrojUnosaZaKorisnika(string korisnik)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PurchaseOrderModel Update(PurchaseOrderModel narudzbenica)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
